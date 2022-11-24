b331b5d999f5a90fecf5ab6850f246e98a5e9c21
Mardoqueu Sousa <mdq767@gmail.com>
2022-11-24 16:32:37 -0300

    finishing chat

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function SignIn() {
   return (
     <>
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
+      <p>Desenvolvido por Mardoqueu Sousa</p>
     </>
   )
 
